4ddacbd9b51fcf1d3ca88c8efb3efa58aa29cad9
Nabeelimran <31319555+nabeelimran@users.noreply.github.com>
2023-03-04 16:26:40 +0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,31 @@
 import express from "express";
+const axios = require('axios');
 
 const app = express();
 const port = process.env.PORT || 3333;
 
-app.use(express.json());
-app.use(express.raw({ type: "application/vnd.custom-type" }));
-app.use(express.text({ type: "text/html" }));
+//enabling cors
+app.use(cors());
 
-app.get("/", async (req, res) => {
-  res.json({ body: "Hello world!" });
+//Parse data
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.post("/post", async (req, res) => {
+  //Destructuring response token from request body
+    const {token} = req.body;
+
+  //sends secret key and response token to google
+      await axios.post(
+        `https://www.google.com/recaptcha/api/siteverify?secret=6LdglM8kAAAAAFqsa6Q9Onc6vxwFEcu7Lf9zFBXE&response=${token}`
+        );
+
+  //check response status and send back to the client-side
+      if (res.status(200)) {
+        res.send({status:true});
+      }else{
+        res.send({status:false});
+      }
 });
 
 app.listen(port, () => {
